196c8a6770febc89519f5aaff687574d05d377b6
hnspn <bobmatt911@gmail.com>
2019-03-30 19:10:04 -0400

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,12 @@
 module.exports = {
   siteMetadata: {
     title: `blog`,
+    siteUrl: 'https://honeyspoon.me', // used by sitemap bluign
     description: `this is a blog`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-offline`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sitemap`,
+  ],
 };
