233529a32e06d25a17d10536626fc5131839f6f0
hnspn <bobmatt911@gmail.com>
2019-03-30 15:12:46 -0400

    default files for a gatsby project

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Nav = () => (
+  <div>
+    Navigation
+    <ul>
+      <li>Home</li>
+      <li>Articles</li>
+      <li>Sketches</li>
+      <li>About me</li>
+    </ul>
+  </div>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Nav } from '../components/Nav';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+
+const Index = () => (
+  <div>
+    <Header />
+    <Nav />
+    <main />
+    <Footer />
+  </div>
+);
+export default Index;
